0d9f2a28810e1d5e71a8664814016120b6045e84
jingyeongH <rkskekfkak12@gmail.com>
2015-04-04 12:50:50 +0900

    program 추가~

---
+++ Project_ME/src/gui/Register_Program.java
@@ -0,0 +1,74 @@
+package gui;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import object.Program;
+import database.FileManager;
+
+public class Register_Program extends JDialog implements ActionListener {
+	JButton Add_B = new JButton("추가");
+	JButton Cancel_B = new JButton("취소");
+	
+	JTextField Name_TF = new JTextField(20);
+	JTextField Part_TF = new JTextField(20);
+	JTextField Difficulty_TF = new JTextField(20);
+	JTextField Comment_TF = new JTextField(20);
+	
+	public Register_Program() {
+		Add_B.addActionListener(this);
+		Cancel_B.addActionListener(this);
+		
+		setLayout(new GridLayout(5, 2));
+		//JPanel panel1 = new JPanel(new FlowLayout());
+		add(new JLabel("이름"));
+		add(Name_TF);
+		add(new JLabel("운동 부위"));
+		add(Part_TF);
+		add(new JLabel("난이도"));
+		add(Difficulty_TF);
+		add(new JLabel("비고"));
+		add(Comment_TF);
+		
+		add(Add_B);
+		add(Cancel_B);
+		
+		pack();
+		setModal(true);
+		setVisible(true);
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == Add_B) {
+			//DB부르기 ★수정
+			FileManager filemanager;
+			Program program = new Program();
+			Calendar calender = Calendar.getInstance();
+			
+			program.setID("" + calender.get(Calendar.HOUR_OF_DAY) + calender.get(Calendar.MINUTE) + calender.get(Calendar.SECOND));
+			program.setName(this.Name_TF.getText());
+			program.setDifficulty(this.Difficulty_TF.getText());
+			program.setPartOfBody(this.Part_TF.getText());
+			program.setComment(this.Comment_TF.getText());
+			
+			filemanager = new FileManager();
+			try {
+				filemanager.addProgram(program);
+			} catch (ClassNotFoundException | SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}			
+		}		
+		dispose();
+	}
+}
---
+++ Project_ME/src/gui/TraingProgram.java
@@ -0,0 +1,124 @@
+package gui;
+
+import gui.Register_Program;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import object.Program;
+import database.FileManager;
+
+public class TraingProgram extends JPanel implements ActionListener{
+
+	JTable jtable;
+	JScrollPane scroll;
+	FileManager filemanager;
+
+	Vector<Vector<String>> row = new Vector<Vector<String>>();
+	Vector<String> col = new Vector<String>();
+	
+	String[] colArray = {"운동", "운동부위", "난이도", "비고"};
+	JButton add_B = new JButton("추가");
+	JButton delete_B = new JButton("삭제");
+	
+	
+	public TraingProgram() throws ClassNotFoundException, SQLException
+	{
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	
+		JPanel panel = new JPanel();
+		
+		//행, 열 추가
+		for(int i=0; i<colArray.length; i++)
+			col.add(colArray[i]);	
+		
+		row = getRow();
+		
+		//테이블 생성
+		jtable = new JTable(row, col);
+		scroll = new JScrollPane(jtable);
+		
+		add_B.addActionListener(this);
+		delete_B.addActionListener(this);
+		
+		
+		panel.add(add_B);
+		panel.add(delete_B);
+		add(panel);
+		add(scroll);
+		setSize(500, 550);
+		setVisible(true);
+	}
+	
+	public void Patch(Vector<Vector<String>> New)
+	{
+		row.removeAllElements();
+		row.addAll(New);
+		jtable.updateUI();
+	}
+	
+	
+	public Vector<Vector<String>> getRow() throws ClassNotFoundException, SQLException
+	{
+		Vector<Vector<String>> result = new Vector<Vector<String>>();
+		filemanager = new FileManager();
+		try {
+			Vector<String> vectorString = new Vector<String>();
+			Vector<Program> vectorProgram = filemanager.getAllProgram();
+				
+			for (Program p : vectorProgram) {
+				Vector<String> program = new Vector<String>();
+				
+				program.add(p.getName());
+				program.add(p.getPartOfBody());
+				program.add(p.getDifficulty());
+				program.add(p.getComment());
+						
+				result.add(program);
+			}
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == add_B)  {
+			new Register_Program();
+		}
+		else if(e.getSource() == delete_B) {
+			int index = jtable.getSelectedRow();
+			try {
+				filemanager.delete(filemanager.getAllProgram().get(index).getID(), "program");
+			} catch (ClassNotFoundException | SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (ArrayIndexOutOfBoundsException e2) {
+				JOptionPane.showMessageDialog(null, "삭제할 운동 프로그램이 없습니다.");
+			}
+		}
+		
+		try {
+			Patch(getRow());
+		} catch (ClassNotFoundException | SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+}
+
